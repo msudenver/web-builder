929aa0082eb2d9ae58b41fbbab878787d6eca3bf
Michael Mason <Michael Mason@terminalfour.com>
2016-11-15 19:36:20 +0000

    Homepage news, events, at a glance components

--- development/terminalfour/src/js/script.js
+++ development/terminalfour/src/js/script.js
@@ -1,2 +1,20 @@
 // Add your custom JavaScripts to this file
-/* A function */
+/* A function set the height on a set of elements */
+function heightEqualizer(query) {
+    var els = document.querySelectorAll(query) || 'undefined';
+    if (els) {
+        var h = 0;
+        els.forEach(function(el, idx) {
+            console.log(el.offsetHeight);        
+            if (el.offsetHeight > h) {
+                h = el.offsetHeight;
+            }
+            console.log(h);
+        });
+        if (h > 0) {
+            els.forEach(function(el) {
+                el.style.height = h+'px';
+            });
+        }
+    }
+}
