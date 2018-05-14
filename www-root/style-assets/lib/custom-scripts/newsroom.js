0e46bc45c928e19378ecb3fa34f32be34bfb61c1
Alan Hernandez <hernandez.alan.grey@gmail.com>
2018-05-14 14:56:16 -0600

    adds additional js logic refinement to newsroom.js. adds compiled files

--- www-root/style-assets/lib/custom-scripts/newsroom.js
+++ www-root/style-assets/lib/custom-scripts/newsroom.js
@@ -3,19 +3,22 @@ $(document).ready(function() {
 	var navbarId = $("#newsroom-nav.newsroom-nav-container");
 	var headerElement = $("#nav-h3");
 	var navRight = $("#nav-right");
+	var navLeft = $("#nav-left");
 	var brandLogo = $("#newsroom-logo");
 	$(window).scroll(function() {
 		if( $(window).scrollTop() >= 35) {
 			mainNavId.addClass('navbar-fixed-top');
 			navbarId.css("height", "150px");
 			headerElement.css("display", "none");
-			navRight.css("margin-top", "0px");
+			navRight.css("margin-top", "30px");
+			navLeft.css("padding-top", "50px");
 			brandLogo.css({"max-width" :"120px", "padding-top" :"0px"});
 		} else {
 			mainNavId.removeClass('navbar-fixed-top');
 			navbarId.removeAttr("style");
 			headerElement.removeAttr("style");
 			navRight.removeAttr("style");
+			navLeft.removeAttr("style");
 			brandLogo.removeAttr("style");
 		}
 	});
