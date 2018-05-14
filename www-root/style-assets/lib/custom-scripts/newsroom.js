2fff34bd095fa3256b4896dc58226fe97de86cbc
Alan Hernandez <hernandez.alan.grey@gmail.com>
2018-05-14 14:20:19 -0600

    adds ids to key elements in the nav bar. adds additinoal js logic in newsroommjs. adds compiled files

--- www-root/style-assets/lib/custom-scripts/newsroom.js
+++ www-root/style-assets/lib/custom-scripts/newsroom.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-	console.log("hello world! I am a custom.js script!");
+	var mainNavId = $("#main-news-nav");
+	var navbarId = $("#newsroom-nav.newsroom-nav-container");
+	var headerElement = $("#nav-h3");
+	var navRight = $("#nav-right");
+	var brandLogo = $("#newsroom-logo");
+	$(window).scroll(function() {
+		if( $(window).scrollTop() >= 35) {
+			mainNavId.addClass('navbar-fixed-top');
+			navbarId.css("height", "150px");
+			headerElement.css("display", "none");
+			navRight.css("margin-top", "0px");
+			brandLogo.css({"max-width" :"120px", "padding-top" :"0px"});
+		} else {
+			mainNavId.removeClass('navbar-fixed-top');
+			navbarId.removeAttr("style");
+			headerElement.removeAttr("style");
+			navRight.removeAttr("style");
+			brandLogo.removeAttr("style");
+		}
+	});
 });
+
+// add this to id after scroll .navbar-fixed-top
