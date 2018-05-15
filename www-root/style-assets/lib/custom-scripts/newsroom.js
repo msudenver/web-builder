b9288fb340c205ebf58f8c0844587f0ea65dde2d
Alan Hernandez <hernandez.alan.grey@gmail.com>
2018-05-15 14:02:53 -0600

    adds style js refinments to newsroom.js. adds compiled files

--- www-root/style-assets/lib/custom-scripts/newsroom.js
+++ www-root/style-assets/lib/custom-scripts/newsroom.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var mainNavId = $("#main-news-nav");
 	var navbarId = $("#newsroom-nav.newsroom-nav-container");
+	var navbarCollapse = $("#navbar-collapse-1");
 	var headerElement = $("#nav-h3");
 	var navRight = $("#nav-right");
 	var navLeft = $("#nav-left");
@@ -12,17 +13,21 @@ $(document).ready(function() {
 			navbarId.css("height", "150px");
 			headerElement.css("display", "none");
 			navRight.css("margin-top", "30px");
+			navRight.removeClass("navbar-right");
 			navLeft.css("padding-top", "50px");
 			brandLogo.css({"max-width" :"120px", "padding-top" :"0px"});
 			newsHero.css("padding-top", "158px");
+			navbarCollapse.css({"position": "relative", "right": "-250px"});
 		} else {
 			mainNavId.removeClass('navbar-fixed-top');
+			navRight.addClass("navbar-right");
 			navbarId.removeAttr("style");
 			headerElement.removeAttr("style");
 			navRight.removeAttr("style");
 			navLeft.removeAttr("style");
 			brandLogo.removeAttr("style");
 			newsHero.removeAttr("style");
+			navbarCollapse.removeAttr("style");
 		}
 	});
 });
