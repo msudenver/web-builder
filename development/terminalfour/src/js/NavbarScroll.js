1112a909e3b7eecc63375c24b343ac89c1b9c7d2
TimLucca <tim.lucca88@gmail.com>
2020-02-13 13:45:39 -0700

    removed visible scroll bar for global-nav while retaining functionality

--- development/terminalfour/src/js/NavbarScroll.js
+++ development/terminalfour/src/js/NavbarScroll.js
@@ -17,9 +17,9 @@
 //       .setAttribute("aria-expanded", false);
 //     $('#bs-example-navbar-collapse-1').css('height', 'auto');
 //   } else if (window.innerWidth <= 991 && document.querySelector('#bs-example-navbar-collapse-1').querySelector(
-//       '.navbar-collapse').classList
+//     '.navbar-collapse').classList
 //     .contains('in') && document.querySelector('#bs-example-navbar-collapse-1').querySelector('.navbar-collapse')
-//     .getAttribute("aria-expanded") != 'true') {
+//       .getAttribute("aria-expanded") != 'true') {
 //     document.querySelector('#bs-example-navbar-collapse-1').querySelector('.navbar-collapse')
 //       .setAttribute("aria-expanded", true);
 //     $('#bs-example-navbar-collapse-1').css('height', '80vh');
